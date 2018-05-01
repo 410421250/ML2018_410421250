a5504868ce4035ecf8ae4755a8aaaa5d5ef0255b
410421250 <biglklkl0512@gmail.com>
2018-05-02 03:27:30 +0800

    read png

---
+++ main.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import numpy as np
+
+
+
+
+k1 = Image.open("key1.png")
+k2 = Image.open("key2.png")
+I = Image.open("I.png")
+e  = Image.open("Eprime.png")
+ee  = Image.open("e.png")
+
+print ( k1.format , k1.size , k1.mode )
+
+
+
+        
+
+
+
+

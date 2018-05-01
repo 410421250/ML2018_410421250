d49002991986b98e61f30ae36bbbf036ce9853be
410421250 <biglklkl0512@gmail.com>
2018-05-02 03:50:40 +0800

    png to array

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ ee  = Image.open("e.png")
 
 print ( k1.format , k1.size , k1.mode )
 
+data = np.zeros((120000,3),int)
+e_arr = np.zeros((120000),int)
+ 
+width, height = k1.size
+
+for y in range(height):
+    for x in range(width):
+        data[y*400+x][0] =  I.getpixel((x,y))
+        data[y*400+x][1] =  k1.getpixel((x,y))
+        data[y*400+x][2] =  k2.getpixel((x,y))
+        e_arr[y*400+x] =  e.getpixel((x,y))
+  
+
+
 
 
         
